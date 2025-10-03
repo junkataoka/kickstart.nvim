a6b20299528927f2861189e1e2277a9222a73c8f
Jun Kataoka <junkataoka@Juns-MacBook-Pro.local>
2025-10-03 10:13:02 +0900

    add alt based buffer move

---
+++ lua/custom/plugins/lualine.lua
@@ -0,0 +1,43 @@
+return {
+  'nvim-lualine/lualine.nvim',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  config = function()
+    require('lualine').setup {
+      options = {
+        theme = 'tokyonight',
+        section_separators = { left = '', right = '' },
+        component_separators = { left = '', right = '' },
+      },
+      sections = {
+        lualine_a = { 'mode' },
+        lualine_b = { 'branch', 'diff', 'diagnostics' },
+        lualine_c = { 'filename' },
+        lualine_x = { 'encoding', 'fileformat', 'filetype' },
+        lualine_y = { 'progress' },
+        lualine_z = { 'location' },
+      },
+      inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = { 'filename' },
+        lualine_x = { 'location' },
+        lualine_y = {},
+        lualine_z = {},
+      },
+      tabline = {
+        lualine_a = { 'buffers' },
+        lualine_b = {},
+        lualine_c = {},
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = { 'tabs' },
+      },
+      extensions = {},
+    }
+
+    -- Buffer navigation keybindings
+    vim.keymap.set('n', '<A-l>', ':bnext<CR>', { noremap = true, silent = true, desc = 'Next buffer' })
+    vim.keymap.set('n', '<A-h>', ':bprevious<CR>', { noremap = true, silent = true, desc = 'Previous buffer' })
+    vim.keymap.set('n', '<A-d>', ':bdelete<CR>', { noremap = true, silent = true, desc = 'Delete buffer' })
+  end,
+}
