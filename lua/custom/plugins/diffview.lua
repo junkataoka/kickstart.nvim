2afc53ee39489ec1cbf0229edcac8cede4440793
junkataoka <kataoka0511@gmail.com>
2025-11-02 15:38:29 +0900

    no more diffview

--- lua/custom/plugins/diffview.lua
+++
@@ -1,8 +0,0 @@
-return {
-  'sindrets/diffview.nvim',
-  dependencies = 'nvim-lua/plenary.nvim',
-  cmd = { 'DiffviewOpen' },
-  keys = {
-    { '<leader>gd', '<cmd>DiffviewOpen<cr>', desc = 'DiffView' },
-  },
-}
