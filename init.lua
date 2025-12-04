2d7fff58a42b3949276f3aa5e8694fa9b5933745
junkataoka <kataoka0511@gmail.com>
2025-12-04 13:29:02 +0900

    test

--- init.lua
+++ init.lua
@@ -3,7 +3,7 @@ require 'config.options'
 require 'config.keymaps'
 require 'config.autocmds'
 
--- Install lazy.nvim plugin manager
+--- Install lazy.nvim plugin manager
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
